f5718339eecd268b0218c1fdc734872f28ca32f6
Jack Coy <jackman3000@gmail.com>
2018-08-07 22:14:01 +1000

    Clean up build scripts to wipe public folder first

--- build-dev.sh
+++ build-dev.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -e
+
+rm -rf public
+
 sed 's/const ROOT_URL = .*/const ROOT_URL = "http:\/\/localhost:4000";/g' react-dev/actions/index.js > index.js.tmp && mv index.js.tmp react-dev/actions/index.js
 
 sh ./generate-static.sh &&
--- build-prod.sh
+++ build-prod.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-npm install &&
-bundle install &&
-bundle update &&
-sed 's/const ROOT_URL = .*/const ROOT_URL = "https:\/\/blog.jackcoy.io";/g' react-dev/actions/index.js > index.js.tmp && mv index.js.tmp react-dev/actions/index.js &&
-npm run build &&
-./generate-static.sh &&
+
+set -e
+
+rm -rf public
+
+npm install
+bundle install
+bundle update
+
+sed 's/const ROOT_URL = .*/const ROOT_URL = "https:\/\/blog.jackcoy.io";/g' \
+    react-dev/actions/index.js > index.js.tmp && mv index.js.tmp react-dev/actions/index.js
+
+npm run build
+./generate-static.sh
 bundle exec jekyll build
 
 printf "blog.jackcoy.io" > public/CNAME
