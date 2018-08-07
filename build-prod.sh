464cc8ccf237a2afe2682d87cc3b4febc5e8a640
Jack Coy <jackman3000@gmail.com>
2018-08-07 22:09:59 +1000

    Move ROOT_URL sed to before npm build

--- build-prod.sh
+++ build-prod.sh
@@ -2,8 +2,8 @@
 npm install &&
 bundle install &&
 bundle update &&
-npm run build &&
 sed 's/const ROOT_URL = .*/const ROOT_URL = "https:\/\/blog.jackcoy.io";/g' react-dev/actions/index.js > index.js.tmp && mv index.js.tmp react-dev/actions/index.js &&
+npm run build &&
 ./generate-static.sh &&
 bundle exec jekyll build
 
